e6d414b648beec92df51e1b9090f84f17b67a3e3
Bulbra <darthartem76@gmail.com>
2026-07-29 13:23:24 +0300

    d

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from dotenv import load_dotenv
+from fastapi import FastAPI
+import os
+from TestProxyForTG.src.prox_checker.main import run as start, load_proxies
+app = FastAPI()
+load_dotenv()
+@app.get("/")
+async def home():
+    await asyncio.sleep(20)
+    my_url = f"https://api.telegram.org/bot{os.getenv("BOT_TOKEN")}/getMe"
+    my_concurrency = 100
+    my_timeout = 5
+    proxies = await load_proxies()
+    result = await start(proxies, my_concurrency, my_timeout, url=my_url)
+    return {"result": result}
