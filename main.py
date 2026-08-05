470f3c3e760d553546b2f0cca8410dfb13508e4c
Bulbra <darthartem76@gmail.com>
2026-08-05 13:32:47 +0300

    rename params

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from TestProxyForTG.src.prox_checker.main import run as start, load_proxies
 app = FastAPI()
 load_dotenv()
 @app.get("/")
-async def home(my_concurrency = 100, my_timeout = 5):
+async def home(concurrency = 100, timeout = 5):
     await asyncio.sleep(20)
     my_url = f"https://api.telegram.org/bot{os.getenv("BOT_TOKEN")}/getMe"
     proxies = await load_proxies()
-    result = await start(proxies, my_concurrency, my_timeout, url=my_url)
+    result = await start(proxies, concurrency, timeout, url=my_url)
     return {"result": result}
